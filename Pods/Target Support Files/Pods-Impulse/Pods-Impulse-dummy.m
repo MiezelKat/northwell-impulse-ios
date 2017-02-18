67eeac2f32ea49408e0470f453bc801f9edf8eef
jdkizer9 <james.kizer@gmail.com>
2017-02-17 20:07:47 -0500

    progress

--- Pods/Target Support Files/Pods-Impulse/Pods-Impulse-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Impulse : NSObject
-@end
-@implementation PodsDummy_Pods_Impulse
-@end
--- Pods/Target Support Files/Pods-ImpulseTests/Pods-ImpulseTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ImpulseTests : NSObject
-@end
-@implementation PodsDummy_Pods_ImpulseTests
-@end
